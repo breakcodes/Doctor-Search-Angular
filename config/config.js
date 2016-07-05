eb8d54a82ca55bcc6cc5bb4932d74e5cd3caef33
breakcodes <sahni.gourav0123@gmail.com>
2016-07-05 16:20:53 +0530

    Delete config.js

--- config/config.js
+++
@@ -1,4 +0,0 @@
-//This file contains all the configuration keys 
-exports.port = process.env.PORT || 8085;
-exports.clientId = '93ba986a-a8ac-46f3-8ff5-e1342e955056';
-exports.token = 'OIWGyPh2NhclpKfIfaYCuECIOS4=';
